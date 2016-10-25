5e523249017f335b93cb71581a9f08fdfc4b2dc3
QuentinG3 <QG3393@hotmail.com>
2016-10-25 18:35:18 +0200

    Add perplexities computation and EM algo for linear interpolation

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from text_to_corpus import (get_words_array,
                             pre_process,
-                            n_gram,
-                            ngram_occurence,
                             prediction_dictionnary,
                             predict_with_laplace,
-                            get_sentence_array)
+                            get_sentence_array,
+                            predict_with_linear_interpolation,
+                            compute_perplexity_laplace)
+from n_gram import (n_gram,
+                    ngram_occurence
+                    )
 from printer import print_occurence, print_ngram, print_ngram_occ
 from checker import check_consistency
 import json
@@ -12,13 +15,9 @@ import json
 if __name__ == '__main__':
     train_path = "Dumas/Dumas_train.txt"
     test_path = "Dumas/Dumas_test.txt"
-    train_word_array = get_sentence_array(train_path)
-    with open("output.txt", 'w', encoding='utf-8') as output:
-        for sentence in train_word_array:
-            output.write(str(sentence)+'\n')
-    # test_word_array = pre_process(test_path)
+    train_sentence_array, test_sentence_array = get_sentence_array(train_path,
+                                                                   test_path)
 
-    # print_occurence(word_array, "word_occurence.txt")
     """
     n_gram_dict1 = n_gram(train_word_array, 1)
     n_gram_dict2 = n_gram(train_word_array, 2)
@@ -39,14 +38,24 @@ if __name__ == '__main__':
     print_ngram_occ(ngram_occ2, "./ngramocc/ngramocc2")
     print_ngram_occ(ngram_occ3, "./ngramocc/ngramocc3")
     print_ngram_occ(ngram_occ4, "./ngramocc/ngramocc4")
-    pred_dict = prediction_dictionnary(train_word_array, 2)
-    # print(pred_dict['of']['the'])
-    print(check_consistency(pred_dict, ['i', 'aaa']))
     """
+
     print("Creating dictionnary...")
-    pred_dict = prediction_dictionnary(train_word_array, 5)
+    pred_dict = prediction_dictionnary(train_sentence_array, 5)
+
+    print("Computing perplexity")
+    print(compute_perplexity_laplace(pred_dict, train_sentence_array, 2))
+
+
+
+
+
+
+
     #print("Done creating dictionnary...")
 
     #print(json.dumps(pred_dict, indent=2))
-    print(json.dumps(pred_dict['indeed']['next']['you']['next']['are']['next']['right'], indent=2))
+    #print(json.dumps(pred_dict['indeed']['next']['you']['next']['are']['next']['right'], indent=2))
     #print(check_consistency(pred_dict, ['indeed', 'you', 'are', 'right']))
+
+    #print(predict_with_linear_interpolation(pred_dict, ['<s>', 'what', 'do'], 'you', [0.33, 0.33, 0.33]))
--- text_to_corpus.py
+++ text_to_corpus.py
@@ -1,6 +1,7 @@
 import re
 import codecs
 import json
+import math
 
 
 def get_words_array(path):
@@ -170,18 +171,111 @@ def predict_with_laplace(prediction_dictionnary, previous_words, lookedup_word):
     current_dict = prediction_dictionnary
     for p_word in previous_words:
         if p_word in current_dict:
-            current_dict = current_dict[p_word]
+            current_dict = current_dict[p_word]['next']
         else:
             return 1/len(prediction_dictionnary)
 
     ch = 0
     for key, value in current_dict.items():
-        ch += value
+        ch += value['score']
 
     if lookedup_word in current_dict:
-        return (current_dict[lookedup_word] + 1)/(ch+len(prediction_dictionnary))
+        return ((current_dict[lookedup_word]['score'] + 1)
+                / (ch+len(prediction_dictionnary)))
     else:
         return 1/(ch+len(prediction_dictionnary))
 
-def predict_top_n_word(pred_dict, previous_word):
-    pass
+
+def predict_with_linear_interpolation(pred_dict, previous_word, lookedup_word, lambda_list):
+    total_proba = 0
+
+    for x in range(0, len(lambda_list)):
+        current_previous_word = previous_word[x:]
+        prob = get_proba(pred_dict, current_previous_word, lookedup_word)
+        print(prob)
+        total_proba += lambda_list[x] * prob
+
+    return total_proba
+
+
+def get_proba(pred_dict, previous_word, lookedup_word):
+    current_dict = pred_dict
+
+    for p_word in previous_word:
+
+        if p_word in current_dict:
+            current_dict = current_dict[p_word]['next']
+        else:
+            return 0
+
+    ch = 0
+    for key, value in current_dict.items():
+        ch += value['score']
+
+    if lookedup_word in current_dict:
+        return current_dict[lookedup_word]['score']/ch
+    else:
+        return 0
+"""
+def estimation_maximisation(sentence_array, n):
+    lambda_list = [1/n] * n
+
+    for x in range(20, 40):
+        sentence_train = sentence_array[:(x)] + sentence_array[(x+1):]
+        sentence_test = sentence_array[x]
+
+        pred_dict = prediction_dictionnary(sentence_train, 5)
+
+        for lambd in range(0, len(lambda_list)):
+            expectation = compute_expectation(pred_dict, lambd, lambda_list, sentence_test)
+            lambda_list[lambd] = expectation/len(sentence_array)
+
+    return lambda_list
+
+
+
+def compute_expectation(pred_dict, lamdb, lambda_list, sentence_test):
+    total = 0
+    for x in range(lamdb+1, len(sentence_test)):
+        prev_word = sentence_test[(x-lamdb+1):x]
+        lookedup_word = sentence_test[x]
+        numerator += lambda_list[lamdb] * get_proba(pred_dict, prev_word, lookedup_word)
+        denominator = 0
+        for y in range(0, len(lambda_list)):
+            if(x-y+1) >= 0:
+                prev_word = sentence_test[(x-y+1):x]
+                lookedup_word = sentence_test[x]
+                denominator += lambda_list[y] * get_proba(pred_dict,
+                                                          prev_word,
+                                                          lookedup_word)
+
+        total += numerator/denominator
+"""
+
+
+def compute_perplexity_laplace(pred_dict, sentence_array, n):
+    m = 0
+    total_perplexity = 0
+    out_of_vocab = 0
+
+    current_sentence = 0
+    print(len(sentence_array))
+    for sentence in sentence_array:
+        print(total_perplexity)
+        print(out_of_vocab)
+        print(m)
+        current_sentence += 1
+        print(str(current_sentence) + "/" + str(len(sentence_array)))
+        for wordIndex in range(n, len(sentence)):
+            if sentence[wordIndex] == '<UNK>':
+                out_of_vocab += 1
+            m += 1
+            previous_word_array = list()
+            for previous_word_index in range(wordIndex-n, n):
+                previous_word_array.append(sentence[previous_word_index])
+            proba = predict_with_laplace(pred_dict,
+                                         previous_word_array,
+                                         sentence[wordIndex])
+            total_perplexity += math.log(proba, 2)
+
+    return 2**(-1*(total_perplexity/m)), out_of_vocab/m
